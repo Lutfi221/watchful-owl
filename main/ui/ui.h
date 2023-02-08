dbfe5ed94eb0867ec70e4550766a67179fd97008
Lutfi Azis <17lutfi21@gmail.com>
2023-02-08 13:26:32 +0700

    add forgotten include guard

--- main/ui/ui.h
+++ main/ui/ui.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_UI_UI
+#define MAIN_UI_UI
 #include <string>
 #include <vector>
 #include "ftxui/component/screen_interactive.hpp"
@@ -6,3 +8,5 @@ int promptSelection(ftxui::ScreenInteractive *screen,
                     std::vector<std::string> *entries,
                     std::string title = "",
                     std::string description = "");
+
+#endif /* MAIN_UI_UI */
