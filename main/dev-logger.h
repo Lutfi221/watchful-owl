846dd79481c0ccaa2d3cffb1eb27a5c4e013483c
Lutfi Azis <17lutfi21@gmail.com>
2023-02-10 15:13:44 +0700

    Add logging macros

---
+++ main/dev-logger.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_DEV_LOGGER
+#define MAIN_DEV_LOGGER
+
+#include "spdlog/spdlog.h"
+
+#define DEBUG spdlog::debug
+#define INFO spdlog::info
+#define WARN spdlog::warn
+#define ERROR spdlog::error
+
+#endif /* MAIN_DEV_LOGGER */
