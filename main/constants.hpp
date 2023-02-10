ff588b581d6ac5d146e75ad7e55a7e862e3f1013
Lutfi Azis <17lutfi21@gmail.com>
2023-02-10 14:52:03 +0700

    setup basic spdlog

--- main/constants.hpp
+++ main/constants.hpp
@@ -1,10 +1,15 @@
 #ifndef MAIN_CONSTANTS
 #define MAIN_CONSTANTS
+#include <filesystem>
 #include <string>
+
+#include "helpers.h"
 
 namespace constants
 {
     const std::string PERPETUAL_EXE_FILENAME = std::string(PERPETUAL_TARGET_NAME) + u8".exe";
+    const std::filesystem::path LOG_OUTPUT_DIR = getExecutableDirPath() /
+                                                 std::filesystem::path("./dev-logs/");
 }
 
 #endif /* MAIN_CONSTANTS */
--- main/main.cpp
+++ main/main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 
 #include "ftxui/component/screen_interactive.hpp"
+#include "spdlog/sinks/rotating_file_sink.h"
+#include "spdlog/spdlog.h"
 
 #include "constants.hpp"
 #include "ui/browser.h"
@@ -11,10 +13,27 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
+    try
+    {
+        auto maxSize = 1048576; // 1 megabit
+        auto maxFiles = 5;
+        auto outputPath = filesystem::canonical(constants::LOG_OUTPUT_DIR /
+                                                filesystem::path("./watchful-owl.log"))
+                              .u8string();
+        auto logger = spdlog::rotating_logger_mt(
+            "main", outputPath, maxSize, maxFiles);
+        spdlog::set_default_logger(logger);
+    }
+    catch (const spdlog::spdlog_ex &ex)
+    {
+        std::cout << "Log init failed: " << ex.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
     auto screen = ftxui::ScreenInteractive::Fullscreen();
     auto config = loadConfig();
     auto mainPage = MainPage(&screen, &config);
     Browser browser = Browser(&screen, &mainPage);
     browser.load();
-    return 0;
+    return EXIT_SUCCESS;
 }
