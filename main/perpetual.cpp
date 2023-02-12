def0954c0130dbf2ef27f2f43f9479d0ed774852
Lutfi Azis <17lutfi21@gmail.com>
2023-02-12 14:40:56 +0700

    fix: Error when owl output directory does not exist

--- main/perpetual.cpp
+++ main/perpetual.cpp
@@ -1,4 +1,5 @@
 #include <Windows.h>
+#include <filesystem>
 
 #include "config.h"
 #include "helpers.h"
@@ -11,6 +12,13 @@ int main(int argc, char **argv)
     FreeConsole();
     killOtherPerpetualInstances();
     auto config = loadConfig();
+
+    auto outDirPath = filesystem::weakly_canonical(
+        getExecutableDirPath() /
+        filesystem::path(config.outDir));
+    if (!filesystem::exists(outDirPath))
+        filesystem::create_directories(outDirPath);
+
     while (true)
     {
         captureAndAppend(config);
