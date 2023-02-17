090a9237c88dc69fe407841113ce96c2c69a28fb
Lutfi Azis <17lutfi21@gmail.com>
2023-02-17 19:37:14 +0700

    refactor: pages in ui

--- main/ui/ui.cpp
+++ main/ui/ui.cpp
@@ -70,57 +70,58 @@ public:
     AutorunConfigPage(ftxui::ScreenInteractive *screen, Config *config)
         : Page(screen, config, u8"AutorunConfig"){};
 
-    NavInstruction load()
+    NavInstruction load();
+};
+NavInstruction AutorunConfigPage::load()
+{
+    NavInstruction navInstruction;
+    AutorunStatus autorunStatus = getAutorunStatus();
+    std::string description;
+    std::vector<std::string> entries = {"", "Back"};
+    switch (autorunStatus)
     {
-        NavInstruction navInstruction;
-        AutorunStatus autorunStatus = getAutorunStatus();
-        std::string description;
-        std::vector<std::string> entries = {"", "Back"};
-        switch (autorunStatus)
-        {
-        case AutorunDisabled:
-            INFO("Autorun is disabled");
-            entries[0] = "Enable Autorun";
-            description = "Autorun is NOT enabled for the current user. "
-                          "Watchful Owl will NOT start automatically "
-                          "when the computer turns on.";
-            break;
-        case AutorunInvalid:
-            INFO("Autorun is invalid");
-            entries[0] = "Fix Invalid Autorun and Enable It";
-            description = "Autorun is NOT VALID.\n"
-                          "This might be caused by the following reasons:\n"
-                          " - The Watchful Owl program was moved to a different"
-                          "location since it was opened.\n"
-                          " - There are two Watchful Owl program in this PC.\n\n"
-                          "Would you like to fix the issue?";
-            break;
-        case AutorunEnabled:
-            INFO("Autorun is enabled");
-            entries[0] = "Disable Autorun";
-            description = "Autorun is ENABLED for the current user. "
-                          "Watchful Owl will automatically "
-                          "start logging when the computer turns on.";
-            break;
-        default:
-            break;
-        }
-        auto s = promptSelection(this->screen, &entries, "Autorun Configuration", description);
+    case AutorunDisabled:
+        INFO("Autorun is disabled");
+        entries[0] = "Enable Autorun";
+        description = "Autorun is NOT enabled for the current user. "
+                      "Watchful Owl will NOT start automatically "
+                      "when the computer turns on.";
+        break;
+    case AutorunInvalid:
+        INFO("Autorun is invalid");
+        entries[0] = "Fix Invalid Autorun and Enable It";
+        description = "Autorun is NOT VALID.\n"
+                      "This might be caused by the following reasons:\n"
+                      " - The Watchful Owl program was moved to a different"
+                      "location since it was opened.\n"
+                      " - There are two Watchful Owl program in this PC.\n\n"
+                      "Would you like to fix the issue?";
+        break;
+    case AutorunEnabled:
+        INFO("Autorun is enabled");
+        entries[0] = "Disable Autorun";
+        description = "Autorun is ENABLED for the current user. "
+                      "Watchful Owl will automatically "
+                      "start logging when the computer turns on.";
+        break;
+    default:
+        break;
+    }
+    auto s = promptSelection(this->screen, &entries, "Autorun Configuration", description);
 
-        if (s == 1)
-        {
-            navInstruction.stepsBack = 1;
-            return navInstruction;
-        }
-
-        navInstruction.flag = NavReload;
-        if (autorunStatus == AutorunDisabled || autorunStatus == AutorunInvalid)
-            enableAutorun();
-        else
-            disableAutorun();
+    if (s == 1)
+    {
+        navInstruction.stepsBack = 1;
         return navInstruction;
     }
-};
+
+    navInstruction.flag = NavReload;
+    if (autorunStatus == AutorunDisabled || autorunStatus == AutorunInvalid)
+        enableAutorun();
+    else
+        disableAutorun();
+    return navInstruction;
+}
 
 class EncryptionConfigPage : public Page
 {
@@ -128,47 +129,48 @@ public:
     const std::string name;
     EncryptionConfigPage(ftxui::ScreenInteractive *screen, Config *config)
         : Page(screen, config, u8"EncryptionConfig"){};
+    NavInstruction load();
+};
 
-    NavInstruction load()
+NavInstruction EncryptionConfigPage::load()
+{
+    Config *config = this->config;
+    NavInstruction navInstruction;
+    auto publicKeyPath = config->encryption.rsaPublicKeyPath;
+    auto encryptionEnabled = config->encryption.enabled;
+
+    std::vector<std::string> entries = {
+        encryptionEnabled
+            ? "Disable Logging Encryption Temporarily"
+            : "Enable Logging Encryption",
+        "Back"};
+
+    int s = promptSelection(
+        screen, &entries, "Encryption Configuration",
+        "Log file encryption prevents other "
+        "people from accessing your log files.");
+
+    switch (s)
     {
-        Config *config = this->config;
-        NavInstruction navInstruction;
-        auto publicKeyPath = config->encryption.rsaPublicKeyPath;
-        auto encryptionEnabled = config->encryption.enabled;
-
-        std::vector<std::string> entries = {
-            encryptionEnabled
-                ? "Disable Logging Encryption Temporarily"
-                : "Enable Logging Encryption",
-            "Back"};
-
-        int s = promptSelection(
-            screen, &entries, "Encryption Configuration",
-            "Log file encryption prevents other "
-            "people from accessing your log files.");
-
-        switch (s)
+    case 0:
+        navInstruction.flag = NavReload;
+        if (encryptionEnabled)
         {
-        case 0:
-            navInstruction.flag = NavReload;
-            if (encryptionEnabled)
-            {
-                config->encryption.enabled = false;
-                saveConfig(config);
-                break;
-            }
-
-            config->encryption.enabled = true;
+            config->encryption.enabled = false;
             saveConfig(config);
-            break;
-
-        default:
-            navInstruction.stepsBack = 1;
             break;
         }
 
-        return navInstruction;
-    };
+        config->encryption.enabled = true;
+        saveConfig(config);
+        break;
+
+    default:
+        navInstruction.stepsBack = 1;
+        break;
+    }
+
+    return navInstruction;
 };
 
 MainPage::MainPage(ftxui::ScreenInteractive *screen, Config *config)
