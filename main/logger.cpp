e986d3a3185d94887941636296b5040ba0e9d451
Lutfi Azis <17lutfi21@gmail.com>
2023-02-23 16:50:43 +0700

    Add Logger class

--- main/logger.cpp
+++ main/logger.cpp
@@ -4,41 +4,25 @@
 
 #include "capturer.h"
 #include "config.h"
+#include "dev-logger.h"
 #include "helpers.h"
 #include "json.hpp"
+#include "logger.h"
 
-void appendLogEntry(nlohmann::json entry, Config config, time_t timestamp)
-{
-    namespace fs = std::filesystem;
+#define ENC_LOGFILE_VERSION 'A'
+#define ENC_LOGFILE_SUFFIX ".json.log.enc"
+#define LOGFILE_SUFFIX ".json.log"
 
-    std::string outFilePath;
-    char formattedTime[32];
-    strftime(formattedTime, sizeof(formattedTime), "%Y%m%d", localtime(&timestamp));
-    std::string outFileName = std::string(formattedTime) + ".json.log";
-
-    if (isPathRelative(config.outDir))
-    {
-        auto a = getExecutableDirPath(),
-             b = fs::path(config.outDir),
-             c = fs::path(outFileName);
-        outFilePath = (a / b / c).u8string();
-    }
-    else
-        outFilePath = (fs::path(config.outDir) / fs::path(outFileName)).u8string();
-
-    std::ofstream outFile;
-    outFile.open(outFilePath, std::ios::out | std::ios_base::app);
-    if (outFile.fail())
-        throw std::ios_base::failure(std::strerror(errno));
-    outFile << "\n"
-            << entry.dump();
-}
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#define LITTLE_ENDIAN 1
+#else
+#define LITTLE_ENDIAN 0
+#endif
 
 /// @brief Capture a snapshot
-/// @param config Config
 /// @param timestamp UNIX timestamp
 /// @return Snapshot
-nlohmann::json generateBasicLogEntry(Config config, time_t timestamp)
+nlohmann::json generateBasicLogEntry(time_t timestamp)
 {
     nlohmann::json entry;
     entry["time"] = timestamp;
@@ -59,19 +43,123 @@ nlohmann::json generateBasicLogEntry(Config config, time_t timestamp)
     return entry;
 }
 
-/// @brief Capture snapshot, and append to log file.
-/// @param config Config
-void captureAndAppend(Config config)
+logger::Logger::Logger(Config *config)
+{
+    this->config = config;
+    this->outDir = prepareAndProcessPath(config->outDir);
+
+    if (config->encryption.enabled)
+    {
+        this->asymKey = new crypto::AsymKey();
+        auto publicKeyPath = prepareAndProcessPath(config->encryption.rsaPublicKeyPath);
+        this->asymKey->loadFromFile(crypto::KeyTypePublic, publicKeyPath.u8string());
+
+        if (!this->asymKey->validate(crypto::KeyTypePublic))
+            throw crypto::CryptoError("Invalid public key");
+    }
+};
+
+void logger::Logger::captureAndAppend()
 {
     time_t timestamp = time(nullptr);
+    char outBuffer[32];
+    std::string logPath = this->prepareLogFile(timestamp);
+
+    auto logEntry = this->capture(timestamp, getDurationSinceLastInput());
+    this->append(logEntry, logPath, this->config->encryption.enabled);
+}
+
+nlohmann::json logger::Logger::capture(time_t timestamp, unsigned int durationSinceLastInput)
+{
     nlohmann::json entry;
-    unsigned int durationSinceLastInput = getDurationSinceLastInput();
-    if (durationSinceLastInput > config.idleThreshold)
+
+    if (durationSinceLastInput > this->config->idleThreshold)
     {
+        DEBUG("User is away. Last input is {} seconds ago.", durationSinceLastInput);
         entry["timestamp"] = timestamp;
         entry["durationSinceLastInput"] = durationSinceLastInput;
     }
     else
-        entry = generateBasicLogEntry(config, timestamp);
-    appendLogEntry(entry, config, timestamp);
+        entry = generateBasicLogEntry(timestamp);
+
+    return entry;
+}
+
+void logger::Logger::append(nlohmann::json entry, std::string logPath, bool encryptedBinary)
+{
+    std::ofstream logFile;
+    logFile.open(logPath, std::ios::out | std::ios_base::app);
+    if (logFile.fail())
+        throw std::ios_base::failure(std::strerror(errno));
+
+    if (!encryptedBinary)
+        logFile << "\n"
+                << entry.dump();
+
+    std::string jsonStr(entry.dump());
+    this->appendBinary(DataTypeJson,
+                       (unsigned char *)jsonStr.c_str(),
+                       jsonStr.size(), &logFile);
+}
+
+void logger::Logger::appendBinary(logger::DataType type, unsigned char *data,
+                                  size_t dataLen, std::ofstream *fileStream)
+{
+    if (dataLen > 16777215)
+        throw std::runtime_error("Data exceeds supported length of 16 megabytes");
+
+    unsigned char entryLen[3];
+
+#if LITTLE_ENDIAN == 1
+
+    unsigned char *src = (unsigned char *)&dataLen;
+    unsigned char *dst = &entryLen[0];
+
+    // If we assume the `sizeof entryLen` is 3,
+    // then  `i` will go {0, 1, 2},
+    // while `j` will go {2, 1, 0}.
+    for (int i = 0; i < sizeof entryLen; i++)
+    {
+        int j = sizeof entryLen - i - 1;
+        memcpy(dst + j, src + i, 1);
+    }
+#else
+    memcpy(&entryLen, &dataLen, sizeof entryLen);
+#endif
+
+    fileStream->write((char *)&type, 1);
+    fileStream->write((char *)entryLen, sizeof entryLen);
+    fileStream->write((char *)data, dataLen);
+};
+
+std::string logger::Logger::prepareLogFile(time_t timestamp)
+{
+    char outBuffer[10];
+    strftime(outBuffer, sizeof(outBuffer), "%Y%m%d", localtime(&timestamp));
+    std::string date(outBuffer);
+    DEBUG("Prepare log file at timestamp {} and date {}", timestamp, date);
+
+    if (!config->encryption.enabled)
+        return (this->outDir / std::filesystem::path(date + LOGFILE_SUFFIX)).u8string();
+
+    auto logPath = this->outDir / std::filesystem::path(date + ENC_LOGFILE_SUFFIX);
+    if (std::filesystem::exists(logPath))
+    {
+        DEBUG("Encrypted log file for the day already exists.");
+        return logPath.u8string();
+    }
+
+    DEBUG("Create encrypted log file for the day and "
+          "put a version specifier on the first byte.");
+    this->lastAppendDate = date;
+    std::ofstream f(logPath, std::ios::binary);
+    f << ENC_LOGFILE_VERSION;
+
+    return logPath.u8string();
+}
+
+logger::Logger::~Logger()
+{
+    delete this->asymKey;
+    delete this->rotatingSymKey;
 }
--- main/logger.h
+++ main/logger.h
@@ -1,13 +1,63 @@
 #ifndef MAIN_LOGGER
 #define MAIN_LOGGER
+#include <filesystem>
 #include <time.h>
 
 #include "json.hpp"
 
 #include "config.h"
+#include "crypto.h"
 
 nlohmann::json generateBasicLogEntry(Config config, time_t timestamp);
-void captureAndAppend(Config config);
-void appendLogEntry(nlohmann::json entry, Config config, time_t timestamp);
 
+namespace logger
+{
+    enum DataType
+    {
+        DataTypeJson = 0,
+        DataTypeSymKey = 1
+    };
+
+    class Logger
+    {
+    private:
+        /// @brief RSA key to encrypt rotating AES keys.
+        crypto::AsymKey *asymKey = nullptr;
+        /// @brief AES key to encrypt log entries.
+        crypto::SymKey *rotatingSymKey = nullptr;
+        Config *config = nullptr;
+
+        /// @brief How many logs since the last AES key generation.
+        unsigned int logsSinceLatestKeyGen = 0;
+        /// @brief The date (in YYYYMMDD format) of the last append.
+        std::string lastAppendDate;
+        /// @brief Path to the log directory.
+        std::filesystem::path outDir;
+
+        /// @brief Get the appropriate log file name. If encryption is enabled,
+        ///        will create a new encrypted log file for the day (if it doesn't exist)
+        ///        and put a version specifier on the first byte.
+        /// @param timestamp Unix timestamp
+        /// @return Full log file path
+        std::string prepareLogFile(time_t timestamp);
+        void appendBinary(DataType type, unsigned char *data, size_t dataLen, std::ofstream *fileStream);
+
+    public:
+        Logger(Config *config);
+        ~Logger();
+
+        void captureAndAppend();
+        /// @brief Capture a log snapshot.
+        /// @param timestamp Current time in UNIX
+        /// @param durationSinceLastInput Seconds since last user input or interaction
+        /// @return JSON log entry
+        nlohmann::json capture(time_t timestamp, unsigned int durationSinceLastInput = 0);
+
+        /// @brief Append a json entry to the log file.
+        /// @param entry JSON entry
+        /// @param logPath Log path
+        /// @param encryptedBinary Should it be encrypted?
+        void append(nlohmann::json entry, std::string logPath, bool encryptedBinary = false);
+    };
+}
 #endif /* MAIN_LOGGER */
